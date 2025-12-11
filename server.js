e2890f10508f70e9bd66cf5841917d910342f8ad
Greeshma Madala <83536924+Greeshma22000@users.noreply.github.com>
2025-12-11 15:35:44 +0530

    .

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import adminRoutes from "./routes/admin.route.js";
 
 dotenv.config();
 const app = express();
@@ -10,6 +11,8 @@ app.use(cors());
 app.use(express.json());
 
 connectDB();
+
+app.use("/api/admin", adminRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on POrt ${PORT}!...✅✅`));
