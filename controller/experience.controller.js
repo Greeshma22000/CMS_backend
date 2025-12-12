58d2f010d904c03700617d46c9d92af3818b8a23
Greeshma Madala <83536924+Greeshma22000@users.noreply.github.com>
2025-12-12 14:43:26 +0530

    .

---
+++ controller/experience.controller.js
@@ -0,0 +1,37 @@
+import Experience from "../models/Experience.js";
+
+export const createExperience = async(req, res) => {
+    try {
+        const experience = await Experience.create(req.body);
+        res.json(experience);
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+};
+
+export const getExperience = async(req, res) => {
+    try {
+        const data = await Experience.find();
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+};
+
+export const updateExperience = async (req, res) => {
+    try {
+        const experience = await Experience.findByIdAndUpdate(req.params.id, req.body, {new: true});
+        res.json(experience);
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+};
+
+export const deleteExperience = async (req, res) => {
+    try {
+        await Experience.findByIdAndDelete(req.params.id);
+        res.json({message: "Deleted"});
+    } catch (error) {
+        res.json(500).json({error: error.message});
+    }
+};
