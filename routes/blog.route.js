944086b33ec59f5ef4e3870160c6036cbd4a3561
Greeshma Madala <83536924+Greeshma22000@users.noreply.github.com>
2025-12-12 15:46:33 +0530

    .

--- routes/blog.route.js
+++ routes/blog.route.js
@@ -1,5 +1,11 @@
 import express from "express";
+import {createBlog, deleteBlog, getBlog, updateBlog} from "../controller/blog.controller.js";
 
 const router = express.Router();
 
-router.post("/", create)
+router.post("/", createBlog);
+router.get("/", getBlog);
+router.put("/:id", updateBlog);
+router.delete("/:id", deleteBlog);
+
+export default router;
