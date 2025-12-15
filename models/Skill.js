bd5ae616471609cad822f3fbcfac8450ac7bc667
Greeshma Madala <83536924+Greeshma22000@users.noreply.github.com>
2025-12-15 17:13:48 +0530

    .

--- models/Skill.js
+++ models/Skill.js
@@ -1,9 +1,16 @@
 import mongoose, { mongo } from "mongoose";
 
 const skillSchema = new mongoose.Schema({
-    name: String,
-    level: String,
-    icon: String,
+    name: {
+        type: String,
+        required: [true, "Skill name is required"],
+        trim: true
+    },
+    level: {
+        type: String,
+        required: [true, "Skill level is required"],
+        trim: true
+    }
 });
 
 export default mongoose.model("Skill", skillSchema);
